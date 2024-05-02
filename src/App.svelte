dbb9d366e600840ac711d7e822698618ebc87bba
Andrii Matenka <matenka.andrii@gmail.com>
2024-05-02 13:43:55 +0300

    added feedback list/item/form components

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,38 @@
-<script lang="ts">
+<script>
+  import FeedbackList from './components/FeedbackList.svelte';
+  import FeedbackStats from './components/FeedbackStats.svelte';
+  import FeedbackFrom from './components/FeedbackFrom.svelte';
 
+  let feedback = [
+    {
+      id: 1,
+      rating: 10,
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+    },
+    {
+      id: 2,
+      rating: 9,
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+    },
+    {
+      id: 3,
+      rating: 8,
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+    },
+  ];
+
+  $: count = feedback.length;
+  $: average = feedback.reduce((a, { rating }) => a + rating, 0) / feedback.length;
+
+  const deleteFeedback = (e) => {
+    const itemId = e.detail;
+
+    feedback = feedback.filter(o => o.id !== itemId);
+  };
 </script>
 
-<main>
-  <h1>Hello</h1>
+<main class="container">
+  <FeedbackFrom />
+  <FeedbackStats {count} {average} />
+  <FeedbackList {feedback} on:delete-feedback={deleteFeedback} />
 </main>
---
+++ src/components/FeedbackList.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    import FeedbackItem from './FeedbackItem.svelte';
+
+    interface Props {
+        id: number,
+        rating: number,
+        text: string
+    }
+    export let feedback: Props[] = [];
+</script>
+
+{#each feedback as fb (fb.id)}
+    <FeedbackItem item={fb} on:delete-feedback />
+{/each}
